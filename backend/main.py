a0f155886cd12f1b34b883b0417d9afc09f22a50
07ReuelBodhak <reuelbodhak07@gmail.com>
2026-08-23 03:50:49 +0530

    Fix token whitespace causing 401 Unauthorized

--- backend/main.py
+++ backend/main.py
@@ -598,5 +598,5 @@ api_thread.start()
 
 
 threading.Thread(target=caspian_client.listen, daemon=True).start()
-discord_client.run(os.getenv("DISCORD_BOT_TOKEN").strip("\"'"))
+discord_client.run(os.getenv("DISCORD_BOT_TOKEN").strip().strip("\"'"))
 
